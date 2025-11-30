98b8d28d1439f850ae57d61979ae3c65df59fe6d
pestraevayu-yulche <pestraevayu@gmail.com>
2025-11-30 23:36:59 +0500

    Update api.php

--- public/api.php
+++ public/api.php
@@ -6,13 +6,14 @@ include("dbconnect.php");
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
+
 try {
     // Выполняем запрос к базе данных для получения направлений
     $stmt = $pdo->query("SELECT * FROM directions ORDER BY id");
     $directions = $stmt->fetchAll();
-    
+
     // Возвращаем данные в формате JSON
-    echo json_encode($directions);
+    echo json_encode($directions, JSON_UNESCAPED_UNICODE);
     
 } catch (PDOException $e) {
     // В случае ошибки возвращаем пустой массив
